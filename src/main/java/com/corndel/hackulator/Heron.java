016782dc77328e4e254b562253e754709d027642
Charlie-robin <charlie.robin.richardson@gmail.com>
2024-09-12 08:52:20 +0100

    (feat):completed exercises

--- src/main/java/com/corndel/hackulator/Heron.java
+++ src/main/java/com/corndel/hackulator/Heron.java
@@ -3,10 +3,12 @@ package com.corndel.hackulator;
 public class Heron {
 
   /**
-   * Uses Heron's formula to calculate the area of a triangle with sides of length a, b, c
+   * Uses Heron's formula to calculate the area of a triangle with sides of length
+   * a, b, c
    * https://en.wikipedia.org/wiki/Heron%27s_formula
    */
   static double heron(double a, double b, double c) {
-    return Math.sqrt(0.0);
+    double semiPerimeter = (a + b + c) / 2;
+    return Math.sqrt(semiPerimeter * (semiPerimeter - a) * (semiPerimeter - b) * (semiPerimeter - c));
   }
 }
--- src/main/java/com/corndel/hackulator/Subtract.java
+++ src/main/java/com/corndel/hackulator/Subtract.java
@@ -4,23 +4,24 @@ public class Subtract {
   /**
    * Returns the positive difference (or zero)
    *
-   * <p>subtract(5, 2) => 3
+   * <p>
+   * subtract(5, 2) => 3
    *
-   * <p>subtract(3, 7) => 4
+   * <p>
+   * subtract(3, 7) => 4
    *
-   * <p>subtract(5, 5) => 0
+   * <p>
+   * subtract(5, 5) => 0
    *
-   * <p>subtract(-2, 4) => 6
+   * <p>
+   * subtract(-2, 4) => 6
    */
   static int subtract(int x, int y) {
     if (x < y) {
       return y - x;
-    }
-
-    if (y < x) {
+    } else {
       return x - y;
     }
 
-    throw new Error("Is this reachable?");
   }
 }
